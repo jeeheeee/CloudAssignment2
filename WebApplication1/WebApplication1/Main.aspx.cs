36d0e0bdb0aac1c10b6d18f8bb3e0c17298884f2
jeeheeee <31935413+jeeheeee@users.noreply.github.com>
2017-10-04 16:11:01 -0400

    Database
    SQL

---
+++ WebApplication1/WebApplication1/Main.aspx.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+
+
+namespace WebApplication1
+{
+    public partial class Main : System.Web.UI.Page
+    {
+        public void Page_Load(object sender, EventArgs e)
+        {
+            rememberBtn.Click += new EventHandler(btnRemember_Click);
+            btnOrder.Click += new EventHandler(btnOrder_Click);
+        }
+
+        public void btnRemember_Click(object sender, EventArgs e)
+        {
+            string LastName = txtLastName.Text;
+            string FirstName = txtFirstName.Text;
+
+            txtComment.Text = "success";
+
+
+            try
+            {
+                SqlConnection connection = new SqlConnection("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+                //SqlConnection connection = new SqlConnection("Data Source=35.188.203.25,1433; Network Library=DBMSSOCN; Initial Catalog=UserInformation; User ID=admin; Password=password12!;");
+                SqlCommand cmd = new SqlCommand("SELECT * FROM UserInformation where LastName = @txtLastName and FirstName = @txtFirstName;", connection);
+
+                cmd.Parameters.AddWithValue("@txtFirstName", FirstName);
+                cmd.Parameters.AddWithValue("@txtLastName", LastName);
+                connection.Open();
+                SqlDataReader ds = cmd.ExecuteReader();
+                if(ds.HasRows)
+                {
+                    while (ds.Read())
+                    {
+                        int DeliveryStatus;
+                        txtLastName.Text = ds.GetString(0);
+                        txtFirstName.Text = ds.GetString(1);
+                        txtPostalcode.Text = ds.GetString(2);
+                        txtPhone.Text = ds.GetString(3);
+                        listProvince.SelectedValue = ds.GetString(4);
+                        foodCheckbox.SelectedValue = ds.GetString(5);
+                        DeliveryStatus = Convert.ToInt32(ds.GetByte(6));
+                        if (DeliveryStatus == 0)
+                        {
+                            RadioButton1.Checked = true;
+                        }
+                        else if (DeliveryStatus == 1)
+                        {
+                            RadioButton2.Checked = true;
+                        }
+                        txtComment.Text = "success";
+                    }
+                }
+                else
+                {
+                    txtComment.Text = "User Not Found";
+                }
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                txtComment.Text = ex.Message;
+            }
+        }
+
+        public void btnOrder_Click(object sender, EventArgs e)
+        {
+            bool check = true;
+            string LastName = txtLastName.Text;
+            string FirstName = txtFirstName.Text;
+            string PostalCode = txtPostalcode.Text;
+            string PhoneNumber = txtPhone.Text;
+            string Province = listProvince.SelectedItem.Text;
+            string Menu = foodCheckbox.SelectedItem.Text;
+            int DeliveryStatus;
+            string Comments = txtComment.Text;
+
+            if(RadioButton1.Checked == true)
+            {
+                check = true;
+            }
+            else if (RadioButton2.Checked == true)
+            {
+                check = false;
+            }
+
+            if(check)
+            {
+                DeliveryStatus = 0;
+            }
+            else
+            {
+                DeliveryStatus = 1;
+            }
+
+            try
+            {
+                SqlConnection connection = new SqlConnection("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+
+                //SqlConnection connection = new SqlConnection("Data Source=35.188.203.25,1433; Network Library=DBMSSOCN; Initial Catalog=UserInformation; User ID=admin; Password=password12!;");
+                SqlCommand cmd = new SqlCommand("INSERT INTO [UserInformation] ([LastName], [FirstName], [PostalCode], [PhoneNumber], [Province], " +
+                "[Menu], [DeliveryOption], [Comment])" +
+                " VALUES ( @LastName, @FirstName, @PostalCode, @PhoneNumber, @Province, @Menu, @DeliveryStatus, @Comments)");
+                {
+                    cmd.Connection = connection;
+                    connection.Open();
+
+
+                    cmd.Parameters.AddWithValue("@LastName", LastName);
+                    cmd.Parameters.AddWithValue("@Firstname", FirstName);
+                    cmd.Parameters.AddWithValue("@PostalCode", PostalCode);
+                    cmd.Parameters.AddWithValue("@PhoneNumber", PhoneNumber);
+                    cmd.Parameters.AddWithValue("@Province", Province);
+                    cmd.Parameters.AddWithValue("@Menu", Menu);
+                    cmd.Parameters.AddWithValue("@DeliveryStatus", DeliveryStatus);
+                    cmd.Parameters.AddWithValue("@Comments", txtComment.Text);
+                    cmd.ExecuteNonQuery();
+                    connection.Close();
+
+                    txtComment.Text = "Success!!";
+
+                }
+            }
+            catch
+            {
+                txtComment.Text = "Connection Failed";
+            }
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Main.aspx.designer.cs
@@ -0,0 +1,222 @@
+﻿//------------------------------------------------------------------------------
+// <자동 생성됨>
+//     이 코드는 도구를 사용하여 생성되었습니다.
+//
+//     파일 내용을 변경하면 잘못된 동작이 발생할 수 있으며, 코드를 다시 생성하면
+//     이러한 변경 내용이 손실됩니다. 
+// </자동 생성됨>
+//------------------------------------------------------------------------------
+
+namespace WebApplication1 {
+    
+    
+    public partial class Main {
+        
+        /// <summary>
+        /// form1 컨트롤입니다.
+        /// </summary>
+        /// <remarks>
+        /// 자동 생성 필드입니다.
+        /// 수정하려면 디자이너 파일에서 코드 숨김 파일로 필드 선언을 이동하세요.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        
+        /// <summary>
+        /// headImage 컨트롤입니다.
+        /// </summary>
+        /// <remarks>
+        /// 자동 생성 필드입니다.
+        /// 수정하려면 디자이너 파일에서 코드 숨김 파일로 필드 선언을 이동하세요.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Image headImage;
+        
+        /// <summary>
+        /// heading 컨트롤입니다.
+        /// </summary>
+        /// <remarks>
+        /// 자동 생성 필드입니다.
+        /// 수정하려면 디자이너 파일에서 코드 숨김 파일로 필드 선언을 이동하세요.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label heading;
+        
+        /// <summary>
+        /// orderTable 컨트롤입니다.
+        /// </summary>
+        /// <remarks>
+        /// 자동 생성 필드입니다.
+        /// 수정하려면 디자이너 파일에서 코드 숨김 파일로 필드 선언을 이동하세요.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Table orderTable;
+        
+        /// <summary>
+        /// firstName 컨트롤입니다.
+        /// </summary>
+        /// <remarks>
+        /// 자동 생성 필드입니다.
+        /// 수정하려면 디자이너 파일에서 코드 숨김 파일로 필드 선언을 이동하세요.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label firstName;
+        
+        /// <summary>
+        /// txtFirstName 컨트롤입니다.
+        /// </summary>
+        /// <remarks>
+        /// 자동 생성 필드입니다.
+        /// 수정하려면 디자이너 파일에서 코드 숨김 파일로 필드 선언을 이동하세요.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtFirstName;
+        
+        /// <summary>
+        /// lastName 컨트롤입니다.
+        /// </summary>
+        /// <remarks>
+        /// 자동 생성 필드입니다.
+        /// 수정하려면 디자이너 파일에서 코드 숨김 파일로 필드 선언을 이동하세요.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lastName;
+        
+        /// <summary>
+        /// txtLastName 컨트롤입니다.
+        /// </summary>
+        /// <remarks>
+        /// 자동 생성 필드입니다.
+        /// 수정하려면 디자이너 파일에서 코드 숨김 파일로 필드 선언을 이동하세요.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtLastName;
+        
+        /// <summary>
+        /// rememberBtn 컨트롤입니다.
+        /// </summary>
+        /// <remarks>
+        /// 자동 생성 필드입니다.
+        /// 수정하려면 디자이너 파일에서 코드 숨김 파일로 필드 선언을 이동하세요.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button rememberBtn;
+        
+        /// <summary>
+        /// postalCode 컨트롤입니다.
+        /// </summary>
+        /// <remarks>
+        /// 자동 생성 필드입니다.
+        /// 수정하려면 디자이너 파일에서 코드 숨김 파일로 필드 선언을 이동하세요.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label postalCode;
+        
+        /// <summary>
+        /// txtPostalcode 컨트롤입니다.
+        /// </summary>
+        /// <remarks>
+        /// 자동 생성 필드입니다.
+        /// 수정하려면 디자이너 파일에서 코드 숨김 파일로 필드 선언을 이동하세요.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtPostalcode;
+        
+        /// <summary>
+        /// phoneNum 컨트롤입니다.
+        /// </summary>
+        /// <remarks>
+        /// 자동 생성 필드입니다.
+        /// 수정하려면 디자이너 파일에서 코드 숨김 파일로 필드 선언을 이동하세요.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label phoneNum;
+        
+        /// <summary>
+        /// txtPhone 컨트롤입니다.
+        /// </summary>
+        /// <remarks>
+        /// 자동 생성 필드입니다.
+        /// 수정하려면 디자이너 파일에서 코드 숨김 파일로 필드 선언을 이동하세요.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtPhone;
+        
+        /// <summary>
+        /// province 컨트롤입니다.
+        /// </summary>
+        /// <remarks>
+        /// 자동 생성 필드입니다.
+        /// 수정하려면 디자이너 파일에서 코드 숨김 파일로 필드 선언을 이동하세요.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label province;
+        
+        /// <summary>
+        /// listProvince 컨트롤입니다.
+        /// </summary>
+        /// <remarks>
+        /// 자동 생성 필드입니다.
+        /// 수정하려면 디자이너 파일에서 코드 숨김 파일로 필드 선언을 이동하세요.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList listProvince;
+        
+        /// <summary>
+        /// food 컨트롤입니다.
+        /// </summary>
+        /// <remarks>
+        /// 자동 생성 필드입니다.
+        /// 수정하려면 디자이너 파일에서 코드 숨김 파일로 필드 선언을 이동하세요.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label food;
+        
+        /// <summary>
+        /// foodCheckbox 컨트롤입니다.
+        /// </summary>
+        /// <remarks>
+        /// 자동 생성 필드입니다.
+        /// 수정하려면 디자이너 파일에서 코드 숨김 파일로 필드 선언을 이동하세요.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CheckBoxList foodCheckbox;
+        
+        /// <summary>
+        /// option 컨트롤입니다.
+        /// </summary>
+        /// <remarks>
+        /// 자동 생성 필드입니다.
+        /// 수정하려면 디자이너 파일에서 코드 숨김 파일로 필드 선언을 이동하세요.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label option;
+        
+        /// <summary>
+        /// RadioButton1 컨트롤입니다.
+        /// </summary>
+        /// <remarks>
+        /// 자동 생성 필드입니다.
+        /// 수정하려면 디자이너 파일에서 코드 숨김 파일로 필드 선언을 이동하세요.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RadioButton RadioButton1;
+        
+        /// <summary>
+        /// RadioButton2 컨트롤입니다.
+        /// </summary>
+        /// <remarks>
+        /// 자동 생성 필드입니다.
+        /// 수정하려면 디자이너 파일에서 코드 숨김 파일로 필드 선언을 이동하세요.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RadioButton RadioButton2;
+        
+        /// <summary>
+        /// comment 컨트롤입니다.
+        /// </summary>
+        /// <remarks>
+        /// 자동 생성 필드입니다.
+        /// 수정하려면 디자이너 파일에서 코드 숨김 파일로 필드 선언을 이동하세요.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label comment;
+        
+        /// <summary>
+        /// txtComment 컨트롤입니다.
+        /// </summary>
+        /// <remarks>
+        /// 자동 생성 필드입니다.
+        /// 수정하려면 디자이너 파일에서 코드 숨김 파일로 필드 선언을 이동하세요.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtComment;
+        
+        /// <summary>
+        /// btnOrder 컨트롤입니다.
+        /// </summary>
+        /// <remarks>
+        /// 자동 생성 필드입니다.
+        /// 수정하려면 디자이너 파일에서 코드 숨김 파일로 필드 선언을 이동하세요.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnOrder;
+    }
+}
